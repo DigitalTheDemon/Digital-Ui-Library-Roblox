ff56d4d721f8651fe03d81f5eb4e7d659a92b98b
DigitalTheDemon <122573085+DigitalTheDemon@users.noreply.github.com>
2023-01-12 18:45:38 -0500

    Update CustomUILib.lua

--- CustomUILib.lua
+++ CustomUILib.lua
@@ -356,7 +356,7 @@ function __CREATELIB__(nameLib, AnimStyle, TpPlayer)
 	Players.PlayerAdded:Connect(onPlayerAdded)
 	--function GetAndRunFunction(FUNCTION)
 		
-	CreateAlert("Current Version Running: " .. VERSION, 99999, 3, 0)
+	--CreateAlert("Current Version Running: " .. VERSION, 99999, 3, 0)
 		
 	--_INITIALIZEUI() -- AT THE END OF UI. DOES NOT NEED TO BE AT THE END OF ALL SCRIPTS
 end
